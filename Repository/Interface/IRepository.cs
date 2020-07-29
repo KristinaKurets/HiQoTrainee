f6e86c75ede778e9702f76258cd1da241ead1035
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-07-29 19:17:22 +0300

    created repositories for data base entities

---
+++ Repository/Interface/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Interface
+{
+    public interface IRepository<TSource> where TSource : class
+    {
+        TSource Read(int id);
+        TSource Create(TSource item);
+        DbSet<TSource> GetDbSet();
+
+    }
+}
---
+++ Repository/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using DB.Context;
+using DB.Entity;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+using System;
+
+namespace Repository.Repositories
+{
+    public class UserRepository : AbstractRepository<User>
+    {
+        public UserRepository(HqrbContext context) : base(context)
+        {
+        }
+
+        public override DbSet<User> GetDbSet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
