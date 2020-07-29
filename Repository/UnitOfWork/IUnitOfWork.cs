01671178a6a9f29dcae3bd92619a321692010b08
Кристина Курец <honey_kris@outlook.com>
2020-07-29 20:05:35 +0300

    UnitOfWorks added

---
+++ Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        TSource GetRepository<TSource>() where TSource : class;
+        void Save();
+    }
+}
---
+++ Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using DB.Context;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.UnitOfWork
+{
+    public class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private HqrbContext db;
+        public UnitOfWork() : this(new HqrbContext()) { }
+
+        public UnitOfWork(HqrbContext dbContext)
+        {
+            db = dbContext;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+        
+        private bool disposed = false;
+       
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+                
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public TSource GetRepository<TSource>() where TSource : class
+        {
+            var result = (TSource)Activator.CreateInstance(typeof(TSource), db);
+            if (result != null)
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
