2ab5bd9f44a8a62cfc3dbc3d614285d525d05e11
Pavel Sas <carminkaa@gmail.com>
2020-08-16 12:15:12 +0300

    Removed Automapper from business services, moved DTO from business services, changed the type of returned data in business services and other minor fixes

--- Service/BookingService/Helpers/DeskStatusHelper.cs
+++ Service/BookingService/Helpers/DeskStatusHelper.cs
@@ -1,33 +1,27 @@
-﻿using AutoMapper;
-using DB.Entity;
+﻿using DB.Entity;
 using DB.EntityStatus;
-using Service.BookingService.DTO;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Service.BookingService.Helpers
 {
     class DeskStatusHelper
     {
         protected readonly DateTime _time;
-        protected readonly IMapper _mapper;
-        public DeskStatusHelper(IMapper mapper,DateTime dateTime)
-        {
-            _mapper = mapper;         
+        public DeskStatusHelper(DateTime dateTime)
+        {      
             _time = dateTime;
         }
 
-        public BookingDeskDTO Count(Desk desk)
+        public Desk Count(Desk desk)
         {
-            BookingDeskDTO resultDesk = _mapper.Map<Desk,BookingDeskDTO>(desk);
+            Desk resultDesk = desk;
             if (desk.Status != DeskStatus.Fixed)
             {
                 var orders = desk.Orders.Where(x => x.Desk==desk && x.DateTime == _time && (x.Status == BookingStatus.Booked || x.Status == BookingStatus.Used));
                 if (orders.Count() != 0)
                 {
-                    resultDesk.User = _mapper.Map <User,BookingUserDTO> (orders.ToList()[0].User);
+                    resultDesk.User = orders.ToList()[0].User;
                     resultDesk.Status = DeskStatus.Booked;
                 }
             }
