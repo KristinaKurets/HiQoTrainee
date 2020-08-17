b3078c3da9eee51d8b6a8ee8e0202ca57d673ddc
diana_aristova <diana.aristova@hiqo-solutions.com>
2020-08-17 16:10:13 +0300

    Add DeskAvaliabilityController.

--- HiQo Remote Booking/Controllers/BookingControllers/DeskAvailabilityController.cs
+++ HiQo Remote Booking/Controllers/BookingControllers/DeskAvailabilityController.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DB.EntityStatus;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service.BookingService.Interfaces;
 
 namespace HiQo_Remote_Booking.Controllers.BookingControllers
 {
     
     [Controller]
-    public class BookingController : ControllerBase
+    public class DeskAvailabilityController : Controller
     {
+        private readonly IDeskAvailabilityService deskAvailabilityService;
+
+        public DeskAvailabilityController(IDeskAvailabilityService deskAvailabilityService)
+        {
+            this.deskAvailabilityService = deskAvailabilityService;
+        }
+
+        [HttpPost]
+        public IActionResult GetDeskAvailability(DateTime date)
+        {
+            return Json(deskAvailabilityService.GetDeskAvailability(date));
+        }
+
+        [HttpPost]
+        public IActionResult GetDeskAvailability(DateTime date, DeskStatus deskStatus)
+        {
+            return Json(deskAvailabilityService.GetDeskAvailability(date, deskStatus));
+        }
     }
 }
