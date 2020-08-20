e51cfabcfdfbb6e67004431d491bbe9d85018eb5
Кристина Курец <honey_kris@outlook.com>
2020-08-20 11:54:22 +0300

    UserNotificationsController addedd

--- HiQo Remote Booking/Controllers/UserNotificatoinsController.cs
+++ HiQo Remote Booking/Controllers/UserNotificatoinsController.cs
@@ -15,15 +15,39 @@ namespace HiQo_Remote_Booking.Controllers
     {
         private readonly IUserNotificationsService _userNotificationsService;
         /// <summary>
-        /// 
+        /// Сonfiguring calendar sync notifications.
         /// </summary>
         /// <param name="userID">The unique User ID.</param>
-        /// <param name="flag">On/off notifications</param>
-        /// <returns>A bool containing the information about flag of notifications</returns>
+        /// <param name="flag">On/off notifications.</param>
+        /// <returns>A bool containing the information about flag of notifications.</returns>
         [HttpGet]
         public IActionResult SetCalendarSyncNotificatios(int userID, bool flag)
         {
             return Json(_userNotificationsService.CalendarSyncNotification(userID,flag));
+        }
+        /// <summary>
+        /// Сonfiguring cancellation booking notifications.
+        /// </summary>
+        /// <param name="userID">The unique User ID.<</param>
+        /// <param name="flag">On/off notifications.</param>
+        /// <returns>A bool containing the information about flag of notifications.</returns>
+        
+        [HttpGet]
+        public IActionResult SetCancellationNotification(int userID, bool flag)
+        {
+            return Json(_userNotificationsService.CancellationNotification(userID, flag));
+        }
+        /// <summary>
+        /// Сonfiguring confirmation booking notifications.
+        /// </summary>
+        /// <param name="userID">The unique User ID.</param>
+        /// <param name="flag">On/off notifications.</param>
+        /// <returns>A bool containing the information about flag of notifications.</returns>
+        
+        [HttpGet]
+        public IActionResult SetConfirmationNotification(int userID, bool flag)
+        {
+            return Json(_userNotificationsService.ConfirmationNotification(userID, flag));
         }
     }
 }
